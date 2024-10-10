b76c9e1576754bd2b9b54faa8a2122141c7ff3dd
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-10-09 21:01:00 -0400

    feat: verify authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,12 +4,13 @@ import { FastifyInstance } from 'fastify'
 import { registerPet } from './controllers/register-pet'
 import { getPetDetails } from './controllers/get-pet-details'
 import { searchPets } from './controllers/search-pets'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/orgs', registerOrganization)
   app.post('/sessions', authenticate)
 
-  app.post('/orgs/:orgId/pets', registerPet)
+  app.post('/orgs/:orgId/pets', { onRequest: [verifyJWT] }, registerPet)
 
   app.get('/pets/:petId', getPetDetails)
   app.get('/pets', searchPets)
